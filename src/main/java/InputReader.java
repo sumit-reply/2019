660b9f11c5bcf52787f0683450a5dba244d78de3
Sumit Aggarwal <s.aggarwal@reply.com>
2020-02-12 17:11:42 +0000

    adding repo

---
+++ src/main/java/InputReader.java
@@ -0,0 +1,86 @@
+package main.java;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+public class InputReader
+{
+	public static void main(final String[] args)
+	{
+		final InputReader reader = new InputReader();
+		final File f = new File("/Users/sumitaggarwal/Documents/REPLYCODECHALLANGE2019/resources/1_victoria_lake.txt");
+		try
+		{
+			final FileInputStream inputStream = new FileInputStream(f);
+			final Scanner sc = new Scanner(inputStream, "UTF-8");
+
+			System.out.println("Start reading file......");
+
+
+			final String firstLine = sc.nextLine();
+			final String[] firstArr = firstLine.split("\\s");
+			int customerOffices = Integer.parseInt(firstArr[2]);
+
+			final int n = Integer.parseInt(firstArr[0]);
+			final int m = Integer.parseInt(firstArr[1]);
+			final char[][] grid = new char[m][n];
+
+			final List<Point> customers = new ArrayList<>();
+
+			while (customerOffices > 0)
+			{
+				final String cutomerOffice = sc.nextLine();
+				final String[] arrCustOffice = cutomerOffice.split("\\s");
+
+				final Point point = new Point(Integer.parseInt(arrCustOffice[0]), Integer.parseInt(arrCustOffice[1]),
+						Integer.parseInt(arrCustOffice[2]));
+				customers.add(point);
+
+				customerOffices--;
+			}
+
+			int countRow = 0;
+
+			while (sc.hasNextLine())
+			{
+
+				final String line = sc.nextLine();
+				for (int i = 0; i < n; i++)
+				{
+					grid[countRow][i] = line.charAt(i);
+				}
+				countRow++;
+			}
+			System.out.println("Finished reading file......");
+		}
+		catch (final FileNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+
+	// get file from classpath, resources folder
+	private File getFileFromResources(final String fileName)
+	{
+
+		final ClassLoader classLoader = getClass().getClassLoader();
+
+		final URL resource = classLoader.getResource(fileName);
+		if (resource == null)
+		{
+			throw new IllegalArgumentException("file is not found!");
+		}
+		else
+		{
+			return new File(resource.getFile());
+		}
+
+	}
+}
---
+++ src/main/java/Point.java
@@ -0,0 +1,15 @@
+package main.java;
+
+public class Point
+{
+	int x;
+	int y;
+	int reward;
+
+	public Point(final int x, final int y, final int reward)
+	{
+		this.x = x;
+		this.y = y;
+		this.reward = reward;
+	}
+}
